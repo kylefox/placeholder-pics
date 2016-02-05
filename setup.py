bd2bca7cd49af6bd6382c98a4e8ccc03e2107b4b
Kyle Fox <kylefox@gmail.com>
2016-02-05 16:37:55 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='placeholder-pics',
+    version='0.0.3',
+    description="Easily generate placeholder pictures for your users.",
+    url="https://github.com/kylefox/placeholder-pics",
+    author="Kyle Fox",
+    author_email="kylefox@gmail.com",
+    license="MIT",
+    packages=['placeholder_pics'],
+    install_requires=['Pillow'],
+    zip_safe=False,
+)
